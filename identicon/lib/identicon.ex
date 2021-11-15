0a33862230869bc6ba42759f90020ed463e1b2e3
Ariel Diaz <arieldiaz92@gmail.com>
2021-11-15 15:33:23 -0500

    before creating image

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -8,6 +8,15 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn ({code, _index} = square) ->
+      rem(code,2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
   end
 
   @doc """
@@ -20,18 +29,25 @@ defmodule Identicon do
     %Identicon.Image{hex: hex}
   end
 
-  def pick_color(%Identicon.Image{hex: [r,g,b | _tail]} = image) do
-    %Identicon.Image{image | color: {r,g,b}}
-  end
-
   def build_grid(%Identicon.Image{hex: hex} = image) do
-    hex
-    |> Enum.chunk_every(3)
+    grid =
+      hex
+      |> Enum.chunk_every(3,3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{ image | grid: grid }
   end
 
   def mirror_row(row) do
     [first, second | _tail] = row
+
     row ++ [second, first]
+  end
+
+  def pick_color(%Identicon.Image{hex: [r,g,b | _tail]} = image) do
+    %Identicon.Image{image | color: {r,g,b}}
   end
 
 end
